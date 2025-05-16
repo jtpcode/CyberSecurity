940e62e6e684f5d0fd5fa5ed9ecb7253c94954c1
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-05-16 11:48:39 +0300

    Create message table (model) in database.

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_public = models.BooleanField(default=True)
