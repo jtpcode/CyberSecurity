ac32483614439bb9e85274e9dbcc2c84a72d0b29
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-05-16 14:24:15 +0300

    Add XSS-vulnerability and fix.

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Message
 
+from django.utils.safestring import mark_safe
+
 # Create your views here.
 
 
 def index(request):
     messages = Message.objects.filter(is_public=True).order_by("-created_at")
+    for msg in messages:
+        msg.content = mark_safe(msg.content)
+    # FIX for XSS: remove the for-loop completely, so messages aren't marked
+    # as safe.
 
     return render(request, "guestbook/index.html", {"messages": messages})
 
@@ -15,7 +21,6 @@ def submit_message(request):
         name = request.POST.get("name")
         email = request.POST.get("email")
         content = request.POST.get("content")
-        # TODO: turva-aukko?
         Message.objects.create(name=name, email=email, content=content)
 
         return redirect("index")
