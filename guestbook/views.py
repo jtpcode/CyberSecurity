697a298b89598a45a519a26a62d68fab706630b2
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-05-16 13:12:03 +0300

    Add views for submit ja index

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Message
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the guestbook index.")
+    messages = Message.objects.filter(is_public=True).order_by("-created_at")
+
+    return render(request, "guestbook/index.html", {"messages": messages})
+
+
+def submit_message(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        content = request.POST.get("content")
+        # TODO: turva-aukko?
+        Message.objects.create(name=name, email=email, content=content)
+
+        return redirect("index")
+    return render(request, "guestbook/submit.html")
